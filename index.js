0b884b8c0e28b267ee648880522535baeb9f75bd
bere abreo <berenicej2013@gmail.com>
2021-04-03 05:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function happyHolidaysTo(name) {
 }
 
 function happyCustomHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!";
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
-  console.log(`It's ${days} days until ${holiday}!`)
+  return `It's ${days} days until ${holiday}!`
 }
